ab38f43b00c418d8a3f9e0826ca8e3d2dea628f5
dongsamb <dongsamb@gmail.com>
2016-01-13 10:25:18 +0900

    delete old dir

--- MERCY/main.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    """solution of https://algospot.com/judge/problem/read/MERCY
-    """
-    input = int(raw_input())
-    for i in range(input):
-        print("Hello Algospot!")
